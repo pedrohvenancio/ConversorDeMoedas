18477164107e91f5ce611a0955f04db3bfe09a9b
Pedro Venâncio <ph.venancio2@gmail.com>
2023-04-13 14:43:11 -0300

    First commit - V1.0 completed successfully

---
+++ public/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/index.css">
+    <title>Conversor de Moedas</title>
+</head>
+<body>
+    <?php
+    //Consumindo API - Configurando URL 
+        $dataHoje = date("m-d-Y");
+        $dataPassada = date("m-d-Y", strtotime("-7 days"));
+        $url = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=\''. $dataPassada .'\'&@dataFinalCotacao=\''. $dataHoje .'\'&$top=1&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,dataHoraCotacao';
+
+    //Traduzindo o JSON e capturando os dados
+        $dados = json_decode(file_get_contents($url), true);
+
+    //Atribuindo o valor do dólar
+        $dolar = $dados["value"][0]["cotacaoCompra"];
+    ?>
+
+    <div class="container">
+        <div class="dolar-div">
+            <p>Dólar<br/>Fonte: BACEN</p>
+            <h2>$ <?= "$dolar" ?></h2>
+        </div>
+        <div class="conversor-div">
+            <form action="./results.php" method="post">
+                <div>
+                    <div class="div-input">
+                        <label for="real">BRL</label>
+                        <input name="real" type="text" placeholder="R$ 00,00">
+                    </div>
+                    <span class="span">=</span>
+                    <div class="div-results">
+                        <label>USD</label>
+                        <span>$ 00,00</span>
+                    </div>
+                </div>
+                <input class="input" type="submit" value="Converter">  
+            </form>
+        </div>
+    </div>
+</body>
+</html>
